d31874bccc58ce9829f6905d6616abbba3670254
ig94 <ignatiustan94@gmail.com>
2022-08-06 00:55:33 +0800

    seed file

--- server/models/seedPosts.js
+++ server/models/seedPosts.js
@@ -1,11 +1,27 @@
 let seedPosts = [
   {
+    caption: "초 근접샷😛",
+    artist: "everglow",
+    member: "E:U",
+    selectedFile: "https://i.imgur.com/mg7O4L3.jpg",
+    likeCount: 2800,
+    createdAt: "2022-07-07T10:56:00.000+08:00",
+  },
+  {
+    caption: "수박바와 핑크퐁의 만남\n🤗",
+    artist: "everglow",
+    member: "Mia",
+    selectedFile: "https://i.imgur.com/tOxpJc0.jpg",
+    likeCount: 2600,
+    createdAt: "2022-07-07T10:24:00.000+08:00",
+  },
+  {
     caption: "glow eyes 🤭",
     artist: "everglow",
     member: "Sihyeon",
     selectedFile: "https://i.imgur.com/FFEm3g4.jpg",
     likeCount: 3200,
-    createdAt: "2022-07-02T22:24:00.000+00:00",
+    createdAt: "2022-07-02T22:24:00.000+08:00",
   },
 ];
 
