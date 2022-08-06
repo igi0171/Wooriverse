9b5845f5e1d30e664dae2e730e0f12fe1185fcae
ig94 <ignatiustan94@gmail.com>
2022-08-07 00:34:53 +0800

    likes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -8,22 +8,51 @@ import {
   Typography,
 } from "@material-ui/core/";
 import FavoriteIcon from "@material-ui/icons/Favorite";
-import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import moment from "moment";
 import { useDispatch } from "react-redux";
 
 import { likePost } from "../../../actions/posts";
 import useStyles from "./styles";
 
-const Post = ({ post }) => {
+const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
   const classes = useStyles();
+  const user = JSON.parse(localStorage.getItem("profile"));
+
+  const Likes = () => {
+    if (post.likes.length + post.likeCount > post.likeCount) {
+      return post.likes.find((like) => like === user?.result?._id) ? (
+        <>
+          <FavoriteIcon fontSize="small" />
+          &nbsp;
+          {post.likes.length + post.likeCount}
+        </>
+      ) : (
+        <>
+          <FavoriteBorderIcon fontSize="small" />
+          &nbsp;{post.likes.length + post.likeCount}
+        </>
+      );
+    }
+    return (
+      <>
+        <FavoriteBorderIcon fontSize="small" />
+        &nbsp;{post.likeCount}
+      </>
+    );
+  };
 
   return (
-    <Card className={classes.card}>
+    <Card className={classes.card} raised elevation={6}>
       <CardMedia className={classes.media} image={post.selectedFile} />
       <div className={classes.overlay}>
-        <Typography variant="h6">{post.member}🌟</Typography>
+        <Typography variant="h6">
+          {post.member}
+          <span role="img" aria-label="Star">
+            🌟
+          </span>
+        </Typography>
         <Typography variant="body2">
           {moment(post.createdAt).format("MM. DD. HH:mm")}
         </Typography>
@@ -37,13 +66,12 @@ const Post = ({ post }) => {
         <Button
           size="small"
           color="primary"
+          disabled={!user?.result}
           onClick={() => {
             dispatch(likePost(post._id));
           }}
         >
-          <FavoriteIcon fontSize="small" />
-          &nbsp;
-          {post.likeCount}
+          <Likes />
         </Button>
       </CardActions>
     </Card>
