3599389153803c920aa4cbfb549dee3506281699
ig94 <ignatiustan94@gmail.com>
2022-08-08 02:02:36 +0800

    details; refactor likes

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardActions,
@@ -6,11 +6,13 @@ import {
   CardMedia,
   Button,
   Typography,
+  ButtonBase,
 } from "@material-ui/core/";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import moment from "moment";
 import { useDispatch } from "react-redux";
+import moment from "moment";
+import { useHistory } from "react-router-dom";
 
 import { likePost } from "../../../actions/posts";
 import useStyles from "./styles";
@@ -19,19 +21,34 @@ const Post = ({ post, setCurrentId }) => {
   const dispatch = useDispatch();
   const classes = useStyles();
   const user = JSON.parse(localStorage.getItem("profile"));
+  const history = useHistory();
+  const [likes, setLikes] = useState(post?.likes);
+
+  const hasLikedPost = post.likes.find((like) => like === user?.result?._id);
+  const userId = user?.result?._id;
+
+  const handleLike = async () => {
+    dispatch(likePost(post._id));
+
+    if (hasLikedPost) {
+      setLikes(post.likes.filter((id) => id !== userId));
+    } else {
+      setLikes([...post.likes, userId]);
+    }
+  };
 
   const Likes = () => {
-    if (post.likes.length + post.likeCount > post.likeCount) {
-      return post.likes.find((like) => like === user?.result?._id) ? (
+    if (likes.length + post.likeCount > post.likeCount) {
+      return likes.find((like) => like === userId) ? (
         <>
           <FavoriteIcon fontSize="small" />
           &nbsp;
-          {post.likes.length + post.likeCount}
+          {likes.length + post.likeCount}
         </>
       ) : (
         <>
           <FavoriteBorderIcon fontSize="small" />
-          &nbsp;{post.likes.length + post.likeCount}
+          &nbsp;{likes.length + post.likeCount}
         </>
       );
     }
@@ -43,33 +60,37 @@ const Post = ({ post, setCurrentId }) => {
     );
   };
 
+  const openPost = () => {
+    history.push(`/posts/${post._id}`);
+  };
+
   return (
     <Card className={classes.card} raised elevation={6}>
-      <CardMedia className={classes.media} image={post.selectedFile} />
-      <div className={classes.overlay}>
-        <Typography variant="h6">
-          {post.member}
-          <span role="img" aria-label="Star">
-            🌟
-          </span>
-        </Typography>
-        <Typography variant="body2">
-          {moment(post.createdAt).format("MM. DD. HH:mm")}
-        </Typography>
-      </div>
-      <CardContent>
-        <Typography variant="body2" component="p">
-          {post.caption}
-        </Typography>
-      </CardContent>
+      <ButtonBase className={classes.cardAction} onClick={openPost}>
+        <CardMedia className={classes.media} image={post.selectedFile} />
+        <div className={classes.overlay}>
+          <Typography variant="h6">
+            {post.member}
+            <span role="img" aria-label="Star">
+              🌟
+            </span>
+          </Typography>
+          <Typography variant="body2">
+            {moment(post.createdAt).format("MM. DD. HH:mm")}
+          </Typography>
+        </div>
+        <CardContent>
+          <Typography variant="body2" component="p">
+            {post.caption}
+          </Typography>
+        </CardContent>
+      </ButtonBase>
       <CardActions className={classes.cardActions}>
         <Button
           size="small"
           color="primary"
           disabled={!user?.result}
-          onClick={() => {
-            dispatch(likePost(post._id));
-          }}
+          onClick={handleLike}
         >
           <Likes />
         </Button>
